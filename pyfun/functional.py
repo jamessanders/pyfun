8e2b8434a8809ea72a85ff7763c937fb33d02160
James Sanders <james.sanders@thisismedium.com>
2010-03-11 13:41:46 -0500

    Initial commit

---
+++ pyfun/functional.py
@@ -0,0 +1,52 @@
+from functional import *
+
+__all__ = ["Fn","curry","combine","call"]
+
+def can_call(obj):
+    if callable(obj): return obj
+    else: raise TypeError("%s is not callable."%obj)
+
+class Fn:
+    """Wrapper class for functional helpers"""
+    def __init__(self,fn):
+        self._fn = can_call(fn)
+
+    def __call__(self,*argv,**kwargs):
+        return self._fn(*argv,**kwargs)
+
+    def __or__(self,f2):
+        return self.combine(f2)
+
+    def __lshift__(self,*argv,**kwargs):
+        try:
+            return self._fn.__call__(*argv,**kwargs)
+        except TypeError:
+            return self.curry(*argv,**kwargs)
+
+    def __mod__(self,*argv,**kwargs):
+        return self.curry(*argv,**kwargs)
+
+    def curry(self,*args,**keywords):
+        """partially apply arguments to a function"""
+        def aux(*fargs,**kwargs):
+            nkw = keywords.copy()
+            nkw.update(kwargs)
+            return self._fn(*(args+fargs),**nkw)
+        return Fn(aux)
+
+    def combine(self,fn):
+        """combine two functions into one"""
+        f = can_call(fn)
+        def aux(*args,**kwargs):
+            return f(self._fn(*args,**kwargs))
+        return Fn(aux)
+    
+def curry(fn,*args,**kwargs):
+    return Fn(fn).curry(*args,**kwargs)
+
+def combine(f1,f2):
+    return Fn(f1).combine(can_call(f2))
+
+def wrapFn(f): return Fn(f)
+
+def call(fn): return can_call(fn)()
